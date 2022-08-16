71cd7565a0cc9ff498744d3ec7842c5670be9422
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-08-16 00:53:32 -0300

    Desafio Menu -- Probando paleta de colores

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>E- commerce de <strong>café</strong></h2>
-        <p>
-          Bueno en está primera entrega, voy a presentar mi idea. Como dice el titulo es un e-commerce de productos de <strong>café</strong> y por su puesto de <strong>café</strong>. Sí, me gusta mucho el <strong>café</strong> ¿Cómo te diste cuenta?
-        </p>
-        <p>Tambien con esta primera presentación me gustaria probar está paleta de colores, ya que, siento que combinan muy bien. Me gustan mucho las paginas con una sola paleta de colores, pero capaz en esta ocasion experimente un poco con los colores. Estoy ansioso por aprender como implementar scss, ya que, ME ENCANTA. Tambien librerias como Animate.css, Three.js, etc</p>
-        <a
-          className="App-link"
-          href="https://joacosangio.github.io/E-commerce/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Proyecto JS
-        </a>
-      </header>
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+const Navbar = () => {
+    return(
+        <header className = "header-container">
+
+        <div className = "logo-tittle">
+            <h1>Coffe Codex</h1>
+
+            <img src = "" alt= "logo"/>
+
+        </div>
+        
+
+        <nav className = "nav-links">
+            <a>Sucursales</a>
+            <a>Productos</a>
+            <a>Nosotros</a>
+            <a><i class="fa-solid fa-cart-shopping-fast"></i></a>
+        </nav>
+            
+
+        </header>
+        
+    )
+}
+
+export default Navbar
