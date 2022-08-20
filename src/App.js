05f52804540d2359fef56bee49495ca3e7e21473
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-08-19 23:43:29 -0300

    Nuevo componente ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './components/Header/Header.scss';
 import "./components/CartWidget/CartWidget.scss"
+import "./components/ItemListContainer/ItemListContainer.scss";
 import Navbar from "./components/Header/Header";
+import ItemListContainer from './components/ItemListContainer/ItemListConteiner';
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ItemListContainer/>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListConteiner.js
@@ -0,0 +1,21 @@
+const ItemListContainer = () => {
+    return(
+       <section className="productos">
+
+        <h2 className="productos-tittle">PRODUCTOS</h2>
+
+            <div className="productos-container">
+
+                <div className="producto">Producto</div>
+                <div className="producto">Producto</div>
+                <div className="producto">Producto</div>
+                <div className="producto">Producto</div>
+                <div className="producto">Producto</div>
+
+            </div>
+
+       </section>
+    )
+}
+
+export default ItemListContainer;
