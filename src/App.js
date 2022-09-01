6e61049e9622d703d4d3c2a0e9ae962a946f4e04
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-09-01 20:58:56 -0300

    Arreglando archivos scss de ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './components/Header/Header.scss';
-import "./components/CartWidget/CartWidget.scss"
-import "./components/ItemListContainer/ItemListContainer.scss";
+import "./components/CartWidget/CartWidget.scss";
 import "./components/Contador/Contador.scss";
+import "./components/ItemList/ItemList.scss";
+import "./components/Item/Item.scss";
 import Navbar from "./components/Header/Header";
 import ItemListContainer from './components/ItemListContainer/ItemListConteiner';
 import Contador from './components/Contador/Contador';
