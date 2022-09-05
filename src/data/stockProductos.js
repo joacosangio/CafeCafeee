d1c7a807dbff5cd3a716cdb80ff671d45a5e39b7
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-09-05 20:57:39 -0300

    Agregando prop Categoria a los productos

--- src/data/stockProductos.js
+++ src/data/stockProductos.js
@@ -6,7 +6,8 @@ export const stockProductos = [
         descripcion: "El mejor café colombiano en grano y de la mas alta calidad",
         precio: 1500,
         stock: 32,
-        alt: "Alt del producto"
+        alt: "Alt del producto",
+        categoria: "Café"
     },
     {
         id: 2,
@@ -15,43 +16,48 @@ export const stockProductos = [
         descripcion: "El mejor café brasilero en grano y de la mas alta calidad",
         precio: 2500,
         stock: 12,
-        alt: "Alt del producto"
+        alt: "Alt del producto",
+        categoria: "Café"
     },
     {
         id: 3,
-        nombre: "Café Arabe",
+        nombre: "Cafetera Oster",
         img: "https://via.placeholder.com/150",
-        descripcion: "El mejor café arabe en grano y de la mas alta calidad",
-        precio: 3500,
+        descripcion: "En pocos minutos y siguiendo simples pasos tu café va a estar listo con resultados profesionales. Programá tu cafetera y empezá a disfrutar de su aroma e intenso sabor.",
+        precio: 75000,
         stock: 22,
-        alt: "Alt del producto"
+        alt: "Alt del producto",
+        categoria: "Cafetera"
     },
     {
         id: 4,
-        nombre: "Café Colombiano",
+        nombre: "Cafetera Nespresso",
         img: "https://via.placeholder.com/150",
-        descripcion: "El mejor café colombiano molido y de la mas alta calidad",
-        precio: 900,
+        descripcion: "Arrancar cada mañana de una forma diferente es posible. Con las cápsulas podés elegir entre una amplia gama de variedades de café y otras bebidas, como chocolate caliente.",
+        precio: 20000,
         stock: 34,
-        alt: "Alt del producto"
+        alt: "Alt del producto",
+        categoria: "Cafetera"
     },
     {
         id: 5,
-        nombre: "Café Brasilero",
+        nombre: "Capsulas Starbucks",
         img: "https://via.placeholder.com/150",
-        descripcion: "El mejor café Brasilero molido y de la mas alta calidad",
+        descripcion: "Capsulas sabor: Capuccino, Caramel, Vainilla o Americano",
         precio: 1500,
-        stock: 19,
-        alt: "Alt del producto"
+        stock: 20,
+        alt: "Alt del producto",
+        categoria: "Capsulas"
     },
     {
         id: 6,
-        nombre: "Café Colombiano",
+        nombre: "Capsulas Nespresso",
         img: "https://via.placeholder.com/150",
-        descripcion: "El mejor café colombiano en grano y de la mas alta calidad",
+        descripcion: "Capsulas sabor: Cosi, Volluto, Capriccio, Livanto, Roma, Arpeggio o Ristretto.",
         precio: 1500,
-        stock: 1,
-        alt: "Alt del producto"
+        stock: 20,
+        alt: "Alt del producto",
+        categoria: "Capsulas"
     },
     
     ]
