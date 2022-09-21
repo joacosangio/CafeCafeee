3bc523754ce8d9a8fde5cfe44405aeee03a48281
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-09-21 20:06:38 -0300

    Añadiendo firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBXwZIEl4fwOwmnm0nyQ9965neP08ixZ_E",
+
+  authDomain: "coffecodex-46063.firebaseapp.com",
+
+  projectId: "coffecodex-46063",
+
+  storageBucket: "coffecodex-46063.appspot.com",
+
+  messagingSenderId: "399090301038",
+
+  appId: "1:399090301038:web:f213e2ca416aa746248e7f"
+
+};
+
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const base = getFirestore(firebaseApp)
